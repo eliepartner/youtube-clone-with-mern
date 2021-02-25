1083beee79ade94717b821711611bab2653ba11b
eliepartner <dev@elieorgel.com>
2021-02-25 02:45:18 -0500

    Add Subscribe Feature

--- client/src/components/views/DetailVideoPage/DetailVideoPage.js
+++ client/src/components/views/DetailVideoPage/DetailVideoPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { List, Avatar, Row, Col } from 'antd';
 import axios from 'axios';
 import SideVideo from './Sections/SideVideo';
+import Subscriber from './Sections/Subscriber';
 
 function DetailVideoPage(props) {
 
@@ -23,30 +24,36 @@ function DetailVideoPage(props) {
             })
     }, [])
 
-    return (
-        <Row>
-            <Col lg={18} xs={24}>
-                <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
-                    <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls></video>
+    if (Video.writer) {
+        return (
+            <Row>
+                <Col lg={18} xs={24}>
+                    <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
+                        <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls></video>
 
-                    <List.Item
-                        actions={[]}
-                    >
-                        <List.Item.Meta
-                            avatar={<Avatar src={ Video.writer && Video.writer.image} />}
-                            title={<a href="https://github.com/eliepartner">{Video.title}</a>}
-                            description={Video.description}
-                        />
-                        <div></div>
-                    </List.Item>
+                        <List.Item
+                            actions={[ <Subscriber userTo={Video.writer._id} userFrom={localStorage.getItem('userId')} /> ]}
+                        >
+                            <List.Item.Meta
+                                avatar={<Avatar src={ Video.writer && Video.writer.image} />}
+                                title={<a href="https://github.com/eliepartner">{Video.title}</a>}
+                                description={Video.description}
+                            />
+                            <div></div>
+                        </List.Item>
 
-                </div>
-            </Col>
-            <Col lg={6} xs={24}>
-                <SideVideo />
-            </Col>
-        </Row>
-    )
+                    </div>
+                </Col>
+                <Col lg={6} xs={24}>
+                    <SideVideo />
+                </Col>
+            </Row>
+        )
+    } else {
+        return (
+            <div>Loading...</div>
+        )
+    }
 }
 
 export default DetailVideoPage
